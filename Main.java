ea4e84f941c3df56c8f3a41b5403c235aea89b67
seoe5580 <seoe5580@wrdsb.ca>
2020-10-03 00:54:10 +0000

    Done Assignment

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Fizz buzz game
+ * @author Eric Seo
  */
 public class Main {
 
@@ -9,7 +10,31 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creates a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // Ask the user for a number
+    System.out.println("Please enter a number to play FizzBuzz");
+
+    // Gets the users input
+    int num = input.nextInt();
+
+    // Gets the number and divides it by 3
+    double numFizz = num % 3;
+
+    // Get the number and divides it by 5
+    double numBuzz = num % 5;
+
+    // tells the user what they have to say according to the number that they chose
+    if (numFizz == 0 && numBuzz ==0){
+      System.out.println("You should say FizzBuzz");
+    } else if (numFizz == 0){
+      System.out.println("You should say Fizz");
+    } else if (numBuzz == 0){
+      System.out.println("You should say Buzz");
+    } else {
+      System.out.println("You should say " + num);
+    }
     
   }
 }
